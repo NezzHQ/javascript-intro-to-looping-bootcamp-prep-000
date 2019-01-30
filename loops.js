c5948a5f48065f042833de3245d8920c0c7a1fbf
Bens Hilaire <benshilaire@gmail.com>
2019-01-30 00:32:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,6 +20,19 @@ function whileLoop(n) {
 }
 
 
+function doWhileLoop(array) {
+  var i = 0;
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do {
+    array = array.pop;
+}   while (array.length > 0 && incrementVariable());
+ return array;
+}
+
+
+
 
 
 
